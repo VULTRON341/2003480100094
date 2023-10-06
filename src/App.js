41c756488be49f673a1c50984fc9eed10ff10a0e
Yash Vardhan Chaturvedi <113501286+VULTRON341@users.noreply.github.com>
2023-10-06 12:09:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import NumberMerger from "./NumberMaanagement";
+
+function App() {
+  return (
+    <div className="App">
+      <NumberMerger />
+    </div>
+  );
+}
+
+export default App;
